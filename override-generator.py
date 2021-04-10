ea97f864ee99812c3ec057df07cf0b1441006e8e
Marcel <m132@users.noreply.github.com>
2021-04-10 19:42:06 +0200

    better output file handling

--- override-generator.py
+++ override-generator.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
-from argparse import ArgumentParser, FileType
+from argparse import ArgumentParser
+from pathlib import Path
 
 from override import OverrideBundle, compile_bundles
 
 
 def main() -> None:
     parser = ArgumentParser('override-generator')
+    parser.add_argument('-f', '--force', action='store_true',
+        help='overwrite the output file if it already exists')
     parser.add_argument('bundles', nargs='+',
         help='paths to bundles to compile', metavar='BUNDLE')
-    parser.add_argument('output', type=FileType(mode='wb'),
+    parser.add_argument('output', type=Path,
         help='output file', metavar='OUTPUT')
 
     args = parser.parse_args()
-    compile_bundles(map(OverrideBundle.from_dir, args.bundles), args.output)
+    try:
+        output = args.output.open('wb' if args.force else 'xb')
+    except OSError as exception:
+        parser.error(str(exception))
+
+    try:
+        compile_bundles(map(OverrideBundle.from_dir, args.bundles), output)
+    finally:
+        output.close()
 
 if __name__ == '__main__':
     main()
